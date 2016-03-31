0d3787f7ca8605bb32ab2e920f5d0074bea7ba49
Matthew Christensen <mchristensen@moosepod.com>
2016-03-31 00:23:05 -0400

    Added management command for terp

--- webterp/models.py
+++ webterp/models.py
@@ -2,15 +2,24 @@ import uuid
 
 from django.db import models
 from django.conf import settings
+from zmachine import interpreter
 
 class Story(models.Model):
 	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	title = models.CharField(max_length=100)
-	story_data = models.TextField()
+	story_data = models.BinaryField()
 	created_by = models.ForeignKey(settings.AUTH_USER_MODEL)
 	created_when = models.DateTimeField(auto_now_add=True)
 
-	def str(self):
+	def get_zmachine(self):
+		terp_story = interpreter.Story(self.story_data)
+		outputs = interpreter.OutputStreams(interpreter.OutputStream(),interpreter.OutputStream())
+		inputs = interpreter.InputStreams(interpreter.InputStream(),interpreter.InputStream())
+		zmachine = interpreter.Interpreter(terp_story,outputs,inputs,None,None)
+		zmachine.reset()
+		return zmachine
+
+	def __str__(self):
 		return '%s (%s)' % (self.title,self.uuid)
 
 	class Meta:
