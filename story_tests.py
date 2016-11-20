272a9e7c35086c536e00e77e5c8bd992a5ed385d
Matthew Christensen <mchristensen@moosepod.com>
2016-11-20 10:38:42 -0500

    Added dump option

--- story_tests.py
+++ story_tests.py
@@ -19,7 +19,7 @@ from zmachine.interpreter import QuitException
 from terp import Terp,load_zmachine
 from curses_terp import StringIOOutputStream,FileInputStream
 
-def test_story(story_path,out_path,commands_path):
+def test_story(story_path,out_path,commands_path,dump):
     print("Starting test of %s" % story_path)
     story_stream = StringIO()
     zmachine = load_zmachine(story_path)
@@ -42,13 +42,15 @@ def test_story(story_path,out_path,commands_path):
     print('Done. Comparing results')
     expected_results = open(out_path,'r').read()
     results = story_stream.getvalue()
+    if dump:
+        print(results)
     if results == expected_results:
         print('OK.')
     else:
         for line in difflib.context_diff(results, expected_results):
              sys.stdout.write(line)  
 
-def run_tests(story_directory):
+def run_tests(story_directory,dump):
     for path in os.listdir(story_directory):
         if path.endswith('.z3') or path.endswith('.z5'):
             filename,ext = os.path.splitext(path)
@@ -57,7 +59,8 @@ def run_tests(story_directory):
             if os.path.exists(out_file) and os.path.exists(commands_file):
                 test_story(os.path.join(story_directory,path), 
                            out_file,
-                           commands_file)
+                           commands_file,
+                           dump=dump)
 
 def main(*args):
     if sys.version_info[0]<3:
@@ -65,9 +68,10 @@ def main(*args):
     
     parser = argparse.ArgumentParser()
     parser.add_argument('directory',help='Directory containing test files ')
+    parser.add_argument('--dump',help='Print test output ',required=False,action='store_true')
     data = parser.parse_args()
 
-    run_tests(data.directory)
+    run_tests(data.directory,dump=data.dump)
 
 if __name__ == "__main__":
     main()
