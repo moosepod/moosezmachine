395c1abcc94b4111fc0889af47bb7de34e04e524
Your Name <you@example.com>
2016-11-27 14:39:32 -0500

    Removed old webterp

--- tools/transcript_tests.sh
+++
@@ -1,6 +0,0 @@
-# Test CZECH.
-CZECH_LOCATION="localtestdata/"
-TRANSCRIPT="/tmp/gamelog.log"
-
-python3 debug.py --file "$CZECH_LOCATION/czech.z3" --transcript /tmp/gamelog.log
-diff --strip-trailing-cr --text $TRANSCRIPT "$CZECH_LOCATION/czech.out3"
--- webterp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebterpConfig(AppConfig):
-    name = 'webterp'
