22ea092d9ae885ab51e4dc04aa4c16845db6b9ba
Matthew Christensen <mchristensen@moosepod.com>
2018-09-03 16:07:41 -0400

    Created pygame version

--- terp.py
+++ terp.py
@@ -1,9 +1,15 @@
 import pygame
 
+from pygame_terp.window import TextWindow
+
 SETTINGS = {'dimensions': (640,480),
+            'char_dimensions': (60,14),
             'font_name': 'courier',
             'font_size': 12}
 
+BLACK_COLOR = (0,0,0)
+WHITE_COLOR = (255,255,255)
+BLUE_COLOR = (0,0,100)
 
 def initialize(settings):
     """ Initialize and open the window """
@@ -12,10 +18,25 @@ def initialize(settings):
     font = pygame.font.SysFont("courier", 12)
     return screen, font
 
-def loop(screen,font):
+def loop(screen,font,settings):
     """ Main game loop """
     done = False
     text = font.render("Hello, World", True, (0, 128, 0))
+
+    status_line_window = TextWindow("Status",
+               screen,
+                font,
+                (0,0),
+                (settings['char_dimensions'][0], 1),
+                WHITE_COLOR,
+                BLACK_COLOR)
+    main_window = TextWindow("Main",
+                screen,
+                font,
+                (0,1),
+                settings['char_dimensions'],
+                BLACK_COLOR,
+                BLUE_COLOR)
 
     while not done:
         for event in pygame.event.get():
@@ -23,11 +44,12 @@ def loop(screen,font):
                 done = True
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 done = True
-    
-        screen.fill((255, 255, 255))
-        screen.blit(text,(20,20))
+
+        screen.fill(WHITE_COLOR)
+        status_line_window.draw()
+        main_window.draw()
         pygame.display.flip()
 
 if __name__ == "__main__":
     screen, font = initialize(SETTINGS)
-    loop(screen,font)
+    loop(screen,font,SETTINGS)
